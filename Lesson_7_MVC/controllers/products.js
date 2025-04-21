2142f5d8e287ae69a64b11818092f00af540f7ec
lauren rigante <93160873+laurenrigante@users.noreply.github.com>
2025-04-21 09:10:15 -0400

    Adding new pages

--- Lesson_7_MVC/controllers/products.js
+++ Lesson_7_MVC/controllers/products.js
@@ -1,23 +1,26 @@
-const Product =require('../models/product');
-exports.getAddProduct=(req,res,next)=>{
-    res.render('add-product', {
-        pageTitle: 'Add Product', 
-        path:'/add-product',
-        formsCSS:true,
-        productCSS:true,
-        activeAddProduct:true
-    });
- };
+const Product = require("../models/product");
+exports.getAddProduct = (req, res, next) => {
+  res.render("admin/add-product", {
+    pageTitle: "Add Product",
+    path: "/add-product",
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true,
+  });
+};
 
- exports.PostAddProduct= (req,res,next)=>{
-     const product=new Product(req.body.title);
-    product.save();
-     res.redirect('/');
-  
-  };
+exports.PostAddProduct = (req, res, next) => {
+  const product = new Product(req.body.title);
+  product.save();
+  res.redirect("/");
+};
 
-  exports.getProducts= (req,res,next)=>{
-    const products=Product.fetchAll((products)=>{
-      res.render('shop', {prods:products, path:'/', pageTitle:'Shop'});
+exports.getProducts = (req, res, next) => {
+  const products = Product.fetchAll((products) => {
+    res.render("shop/product-list", {
+      prods: products,
+      path: "/",
+      pageTitle: "Shop",
     });
-  }
+  });
+};
