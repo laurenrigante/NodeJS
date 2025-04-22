def3cdc8e7f6ddc45e8cdf07f7c8cff80db9e73c
lauren rigante <93160873+laurenrigante@users.noreply.github.com>
2025-04-22 09:20:24 -0400

    implementing the product details page

--- Lesson_9/controllers/shop.js
+++ Lesson_9/controllers/shop.js
@@ -13,9 +13,14 @@ exports.getProducts = (req, res, next) => {
 exports.getProduct=(req,res,next)=>{
   const prodId= req.params.productId; //get the dynamic product id
   Product.findById(prodId, product=>{ //call the method in our model to get the product given an id
-    console.log(product);
+   res.render('shop/product-detail',
+    {
+      product: product, //pass produt to be able to get its propeerties in the view
+      pageTitle:product.title, 
+      path:'/products'
+    },  
+   )
   });
-  res.redirect('/');
 }
 
 
