551bff8b9bf91d6b9e01649771920f49db31d7f6
lauren rigante <93160873+laurenrigante@users.noreply.github.com>
2025-04-23 10:44:37 -0400

    adding .env to gitignore with server variables

--- Lesson_11_Sequelize/util/database.js
+++ Lesson_11_Sequelize/util/database.js
@@ -1,11 +1,12 @@
-const mysql=require('mysql2');
+const Sequelize=require('sequelize');
+require('dotenv').config();
 
-
-const pool= mysql.createPool({
-    host:'localhost',
-    user:'root',
-    database:'node-complete',
-    password:'sqllauren'
-});
-
-module.exports=pool.promise();
+const sequelize=new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        dialect: 'mysql',
+        host: process.env.DB_HOST
+    }
+);
