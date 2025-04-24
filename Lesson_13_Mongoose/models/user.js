ce1d0fc580cadecb5f277e669d8590155f95db4b
lauren rigante <93160873+laurenrigante@users.noreply.github.com>
2025-04-24 13:42:06 -0400

    refactoring adding to cart

--- Lesson_13_Mongoose/models/user.js
+++ Lesson_13_Mongoose/models/user.js
@@ -1,31 +1,57 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const mongoose=require('mongoose');
-const Schema=mongoose.Schema;
-
-const userSchema= new Schema({
-    name:{
-        type:String,
-        required:true
-    },
-    email:{
-        type:String,
-        required:true
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  cart: {
+    items: [
+      {
+        productId: {
+          type: Schema.Types.ObjectId,
+          ref: "Product", //refrence to product model
+          required: true,
+        },
+        quantity: {
+          type: Number,
+          required: true,
+        },
       },
-      cart:{
-        items:[
-            {
-                productId:{
-                    type:Schema.Types.ObjectId,
-                    ref:'Product', //refrence to product model
-                    required:true
-                },
-                quantity:{
-                    type:Number,
-                    required:true
-                }
-            }
-        ]
-      },
+    ],
+  },
 });
 
-module.exports=mongoose.model('User',userSchema); 
+userSchema.methods.addToCart = function (product) {
+  //check if a certain item already exists
+  const cartProductIndex = this.cart.items.findIndex((cp) => {
+    return cp.productId.toString() === product._id.toString(); //compare the ids of the products
+  });
+
+  let newQuantity = 1;
+  const updatedCartItems = [...this.cart.items]; //copy the items in the cart
+
+  if (cartProductIndex >= 0) {
+    newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+    updatedCartItems[cartProductIndex].quantity = newQuantity; //increment the quantity of the product in the cart
+  } else {
+    updatedCartItems.push({
+      productId: product._id,
+      quantity: newQuantity,
+    }); //add the product to the cart
+  }
+
+  const updatedCart = {
+    items: updatedCartItems,
+  };
+
+  this.cart = updatedCart; //update the cart in the user object
+  return this.save();
+};
+
+module.exports = mongoose.model("User", userSchema);
