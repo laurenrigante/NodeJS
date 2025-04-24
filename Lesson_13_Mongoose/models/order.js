c428ef5ad4816d9fee63e289230bd76f52c01744
lauren rigante <93160873+laurenrigante@users.noreply.github.com>
2025-04-24 13:56:06 -0400

    Creatng a order model

---
+++ Lesson_13_Mongoose/models/order.js
@@ -0,0 +1,31 @@
+const mongoose=require('mongoose');
+const Schema=mongoose.Schema;
+
+const orderSchema=new Schema({
+    products:[
+        {
+            product:{
+                type:Schema.Types.ObjectId,
+                required:true,
+            },
+            quantity:{
+                type:Number,
+                required:true
+            }
+        }
+    ],
+    
+    user:{
+        name:{
+            type:String,
+            required:true
+        },
+        userId:{
+            type:Schema.Types.ObjectId,
+            ref:'User', //associations in mongoose, here we are referencing the user model we created
+            required:true
+        }
+    }
+})
+
+const Order=mongoose.model('Order',orderSchema); //mongoose connects a schema to the model
