3ac1cd95b7db8feffec344039d527dd486c8023a
lauren rigante <93160873+laurenrigante@users.noreply.github.com>
2025-04-30 13:14:24 -0400

    hashing the passwords

--- Lesson_15_Auth/controllers/auth.js
+++ Lesson_15_Auth/controllers/auth.js
@@ -1,4 +1,5 @@
 const User = require("../models/user");
+const bcrypt = require("bcryptjs");
 
 exports.getLogin = (req, res, next) => {
   res.render("auth/login", {
@@ -30,39 +31,40 @@ exports.postLogin = (req, res, next) => {
 };
 
 exports.postSignup = (req, res, next) => {
-  console.log('BODY:', req.body); 
+  console.log("BODY:", req.body);
   const email = req.body.email;
   const password = req.body.password;
   const confirmPassword = req.body.confirmPassword;
   //validation implementation later
 
   //check if user already exists
-  User.findOne({ email: email })
-    .then((userDoc) => {
-      if (userDoc) {
-        console.log("User already exists.");
-       return  res.redirect("/signup");
-      }
-      //creaate a new user
-      const user = new User({
-        email: email,
-        password: password,
-        cart: { items: [] },
+  User.findOne({ email: email }).then((userDoc) => {
+    if (userDoc) {
+      console.log("User already exists.");
+      return res.redirect("/signup");
+    }
+
+    //use bcrypt to hash the password
+    return bcrypt
+      .hash(password, 12)
+      .then((hashedPassword) => {
+        //creaate a new user
+        const user = new User({
+          email: email,
+          password: hashedPassword,
+          cart: { items: [] },
+        });
+        console.log("Attempting to save user:", user);
+        return user.save();
+      })
+      .then((result) => {
+        console.log("User saved successfully:", result);
+        res.redirect("/login");
+      })
+      .catch((err) => {
+        console.log("Error saving user:", err);
       });
-      console.log("Attempting to save user:", user);
-      return user.save();
-    })
-    .then((result) => {
-      if (!result) {
-        console.log("No result from user.save(). Possibly redirected earlier.");
-        return;
-      }
-      console.log("User saved successfully:", result);
-      res.redirect("/login");
-    })
-    .catch((err) => {
-      console.log("Error saving user:", err);
-    });
+  });
 };
 
 exports.postLogout = (req, res, next) => {
