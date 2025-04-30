c8e2021f0c82ecfc12a8d3d74051eacc0a3780fd
lauren rigante <93160873+laurenrigante@users.noreply.github.com>
2025-04-30 10:35:32 -0400

    using the session to access user information

--- Lesson_14_Cookies/controllers/auth.js
+++ Lesson_14_Cookies/controllers/auth.js
@@ -1,15 +1,20 @@
-
-exports.getLogin= (req, res, next) => {
-  const isLoggedIn=(req.get('Cookie').split(';')[1].trim().split('='[1])); // Log the cookies sent by the client
-      res.render('auth/login', {
-        pageTitle: 'Login',
-        path: '/login',
-        isAuthenticated: isLoggedIn
-        });
+const User = require("../models/user"); // Import the User model
+exports.getLogin = (req, res, next) => {
+  const isLoggedIn = req.get("Cookie").split(";")[1].trim().split("="[1]); // Log the cookies sent by the client
+  res.render("auth/login", {
+    pageTitle: "Login",
+    path: "/login",
+    isAuthenticated: isLoggedIn,
+  });
 };
 
-exports.postLogin= (req, res, next) => {
-   req.session.isLoggedIn = true; // Set the session variable to indicate the user is logged in
-  // Redirect to the home page
-   res.redirect('/');
-}
+exports.postLogin = (req, res, next) => {
+  //fetch user and store in session
+  User.findById("5bab316ce0a7c75f783cb8a8")
+    .then((user) => {
+      req.session.isLoggedIn = true;
+      req.session.user = user; // Store the user in the session
+      res.redirect("/");
+    })
+    .catch((err) => console.log(err));
+};
