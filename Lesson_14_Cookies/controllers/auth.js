a33241f83a002f4b66898b649cc25ef4fadef460
lauren rigante <93160873+laurenrigante@users.noreply.github.com>
2025-04-30 10:26:37 -0400

    sessions

--- Lesson_14_Cookies/controllers/auth.js
+++ Lesson_14_Cookies/controllers/auth.js
@@ -1,8 +1,15 @@
 
 exports.getLogin= (req, res, next) => {
- 
+  const isLoggedIn=(req.get('Cookie').split(';')[1].trim().split('='[1])); // Log the cookies sent by the client
       res.render('auth/login', {
         pageTitle: 'Login',
-        path: '/login'
+        path: '/login',
+        isAuthenticated: isLoggedIn
         });
 };
+
+exports.postLogin= (req, res, next) => {
+   req.session.isLoggedIn = true; // Set the session variable to indicate the user is logged in
+  // Redirect to the home page
+   res.redirect('/');
+}
