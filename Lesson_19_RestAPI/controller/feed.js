4d56072e51427b292d707ef1732a1158385e424d
lauren rigante <93160873+laurenrigante@users.noreply.github.com>
2025-05-29 13:43:07 -0400

    creating a post request (Testing with postman)

--- Lesson_19_RestAPI/controller/feed.js
+++ Lesson_19_RestAPI/controller/feed.js
@@ -5,3 +5,14 @@ exports.getPosts = (req, res, next) => {
     ],
   });
 };
+exports.postPost = (req, res, next) => {
+  const title = req.body.title;
+  const content = req.body.content;
+
+  //create post in db
+  res.status(201).json({
+    //201 tells the client success a resource was created
+    message: "Post created successfully",
+    post: { id: new Date().toISOString, title: title, content: content },
+  });
+};
--- Lesson_19_RestAPI/routes/feed.js
+++ Lesson_19_RestAPI/routes/feed.js
@@ -5,5 +5,5 @@ const feedController = require("../controller/feed");
 const router = express.Router();
 
 router.get("/posts", feedController.getPosts);
-
+router.post("/post", feedController.postPost);
 module.exports = router;
